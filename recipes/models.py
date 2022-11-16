c597ec2c7256077b99eccf90daf3774264471e32
henrysevern <henrysevern2@gmail.com>
2022-11-16 20:23:10 +0000

    Create model to add recipes to database

--- recipes/models.py
+++ recipes/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+# Model for recipes
 class Recipe(models.Model):
+    # Gives the recipe a maximum title character length of 100 characters
     title = models.CharField(max_length=100)
-    description = models.TextField()
 
+    description = models.TextField()
+    
+    #if a user is deleted so are all the recipes the user created 
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    # Returns the recipe object as its title
+    def __str__(self):
+        return self.title
