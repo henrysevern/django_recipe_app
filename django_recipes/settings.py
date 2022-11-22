94e568645e024c9101c70e4028b9387e4939a62e
henrysevern <henrysevern2@gmail.com>
2022-11-22 10:35:01 +0000

    Add setting to redirect user to home page after login

--- django_recipes/settings.py
+++ django_recipes/settings.py
@@ -132,3 +132,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirects login to recipes home page instead of standard profile url
+LOGIN_REDIRECT_URL = 'recipes-home'
