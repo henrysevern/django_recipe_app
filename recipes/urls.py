76788efb416a849fc9a191f3aedff69e93f0192c
henrysevern <henrysevern2@gmail.com>
2022-11-23 15:11:49 +0000

    Create new url path for recipe create form

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
 
-
-
 urlpatterns = [
+    # url path for all recipes
     path('', views.RecipeListView.as_view(), name="recipes-home"),
-    path('recipe/<int:pk>/', views.RecipeDetailView.as_view(), name="recipes-detail"),
+    # url path for recipe detail
+    path('recipe/<int:pk>/', views.RecipeDetailView.as_view(),
+         name="recipes-detail"),
+    # url path for create recipes
+    path('recipe/create/', views.RecipeCreateView.as_view(),
+         name="recipes-create"),
+    # url path for about page
     path('about/', views.about, name="recipes-about"),
 ]
