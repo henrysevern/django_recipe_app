05551b44608a3b72a54e5c0ee7747818a71a4e68
henrysevern <henrysevern2@gmail.com>
2022-11-24 15:08:40 +0000

    Change text fields to character fields on a few keys

--- recipes/models.py
+++ recipes/models.py
@@ -9,20 +9,20 @@ class Recipe(models.Model):
     # Gives the recipe a maximum title character length of 100 characters
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=200)
-    ingredients = models.TextField(max_length=300, null=True)
-    directions = models.TextField(max_length=300, null=True)
+    ingredients = models.TextField(null=True)
+    directions = models.TextField(null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     featured_image = CloudinaryField('image', default='placeholder')
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    calories = models.TextField(max_length=10, blank=True)
-    carbs = models.TextField(max_length=10, blank=True)
-    fat = models.TextField(max_length=10, blank=True)
-    protein = models.TextField(max_length=10, blank=True)
-    time = models.TextField(max_length=10, blank=True)
-    servings = models.TextField(max_length=10, blank=True)
+    calories = models.CharField()(max_length=10, blank=True)
+    carbs = models.CharField()(max_length=10, blank=True)
+    fat = models.CharField()(max_length=10, blank=True)
+    protein = models.CharField()(max_length=10, blank=True)
+    time = models.CharField()(max_length=10, blank=True)
+    servings = models.CharField()(max_length=10, blank=True)
 
     likes = models.ManyToManyField(User, related_name='recipe_likes',
                                    blank=True)
