c6e8959b5bfa7b17b6519c2871cdb9ae64188990
henrysevern <henrysevern2@gmail.com>
2022-11-26 15:53:31 +0000

    Format document

--- users/views.py
+++ users/views.py
@@ -7,19 +7,19 @@ from . import forms
 
 
 def register(request):
-  if request.method == "POST":
-    form = forms.UserRegisterForm(request.POST)
-    if form.is_valid():
-      form.save()
-      # cleaned data is a dictionary
-      username = form.cleaned_data.get('username')
-      messages.success(request, f"{username}, you're account is created! Please login")
-      return redirect('recipes-home')
-  else:
-    form = forms.UserRegisterForm()
-  return render(request, 'users/register.html', {'form': form})
+    if request.method == "POST":
+        form = forms.UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # cleaned data is a dictionary
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"{username}, you're account is created! Please login")
+            return redirect('recipes-home')
+    else:
+        form = forms.UserRegisterForm()
+        return render(request, 'users/register.html', {'form': form})
 
 
 @login_required
 def profile(request):
-  return render (request, 'users/profile.html')
+    return render(request, 'users/profile.html')
