8adc96e281fafb07be828aee62af75b03099473c
henrysevern <henrysevern2@gmail.com>
2022-11-28 14:18:29 +0000

    Upgrade comment model registration

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 # import models over from models.py
 from . import models
-# Register your models here.
+
+
+@admin.register(models.Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe', 'approved', 'created_on',)
+
+
 admin.site.register(models.Recipe)
-admin.site.register(models.Comment)
+
